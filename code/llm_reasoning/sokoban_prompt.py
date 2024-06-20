e92284b8d071652fd9e944e095443014f6b71d3a
niting <niting1209@gmail.com>
2024-06-20 13:40:55 -0400

    Adding LLM Reasoning Tests

--- code/llm_reasoning/sokoban_prompt.py
+++ code/llm_reasoning/sokoban_prompt.py
@@ -4,24 +4,24 @@ problem_1: str = """
         (:objects sokoban, crate2, l1, l2, l5, l6, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18)
         (:init (sokoban sokoban) 
                (crate crate2)
-    
+
                ;;horizontal relationships
                (leftOf l1 l2) 
                (leftOf l5 l6) 
                (leftOf l9 l10) (leftOf l10 l11) (leftOf l11 l12) 
                (leftOf l13 l14) (leftOf l14 l15) (leftOf l15 l16)
                (leftOf l17 l18)
-    
+
                ;;vertical relationships
                (below l5 l1) (below l6 l2)
                (below l9 l5) (below l10 l6)
                (below l13 l9) (below l14 l10) (below l15 l11) (below l16 l12)
                (below l17 l13) (below l18 l14)
-    
+
                ;;initialize sokoban and crate
                (at sokoban l10)
                (at crate2 l15) 
-    
+
                ;;clear spaces
                (clear l1) 
                (clear l2) 
@@ -35,7 +35,7 @@ problem_1: str = """
                (clear l16) 
                (clear l17)   				
                (clear l18))
-    
+
         (:goal (and (at crate2 l2)))
     )
 """
@@ -101,6 +101,4 @@ For context, here is additional information about the specific action mentioned 
 ```
 
 Using this information, please provide a short, logical response that addresses the question.
----
-
 """
